fa1460fa66527da3f04b527f508929c983d85123
Mikhail <mikhail-zmyslin@yandex.ru>
2022-08-07 17:29:05 +0300

    добавил решение первой задачи

---
+++ task001/Program.cs
@@ -0,0 +1,79 @@
+﻿//Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.Clear();
+Console.WriteLine($"Создаем двумерный массив , заполненный случайными числами.");
+Console.WriteLine($"Упорядочиваем по убыванию элементы каждой строки двумерного массива.");
+Console.WriteLine("");
+Console.WriteLine("Введите количество строк");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("");
+
+
+int[,] Matrix = new int[lines, columns];
+
+void FillingArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100);
+        }
+    }
+}
+
+void SorttedArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                if (Matrix[i, j] <= Matrix[i, k]) continue;
+                int temp = Matrix[i, j];
+                Matrix[i, j] = Matrix[i, k];
+                Matrix[i, k] = temp;
+            }
+    }
+}
+
+
+void PrintArray(int[,] array)
+{
+    Console.WriteLine($"Несортированный массив : ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j].ToString().PadLeft(4) + " | ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+void PrintSortedArray(int[,] array)
+{
+    Console.WriteLine("");
+    Console.WriteLine($"Cортированный массив : ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j].ToString().PadLeft(4) + " | ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+FillingArrayRandomNumbers(Matrix);
+PrintArray(Matrix);
+SorttedArray(Matrix);
+PrintSortedArray(Matrix);
